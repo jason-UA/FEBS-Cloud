be2d542ca0df34bbb6b06c8a0263b9fc45d5841c
Jason Wang <Jason.Wang2@harman.com>
2020-05-17 10:29:37 +0800

    cloud finish

---
+++ febs-server/febs-server-system/src/main/java/com/febs/server/system/FebsServerSystemApplication.java
@@ -0,0 +1,16 @@
+package com.febs.server.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class FebsServerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FebsServerSystemApplication.class, args);
+    }
+}
