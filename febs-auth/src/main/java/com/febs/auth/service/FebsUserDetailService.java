74bdb0c6d1eac6cdbe88e11542c071ceeba03d13
Jason Wang <Jason.Wang2@harman.com>
2020-05-19 21:04:08 +0800

    login finish

--- febs-auth/src/main/java/com/febs/auth/service/FebsUserDetailService.java
+++ febs-auth/src/main/java/com/febs/auth/service/FebsUserDetailService.java
@@ -1,6 +1,10 @@
 package com.febs.auth.service;
 
+import com.febs.auth.manager.Usermanager;
 import com.febs.common.entity.FebsAuthUser;
+import com.febs.common.entity.system.SystemUser;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -16,13 +20,25 @@ public class FebsUserDetailService implements UserDetailsService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private Usermanager usermanager;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        FebsAuthUser user = new FebsAuthUser();
-        user.setUsername(username);
-        user.setPassword(this.passwordEncoder.encode("123456"));
-        return new User(username, user.getPassword(), user.isEnabled(),
-                user.isAccountNonExpired(), user.isCredentialsNonExpired(),
-                user.isAccountNonLocked(), AuthorityUtils.commaSeparatedStringToAuthorityList("user:add"));
+        SystemUser systemUser = usermanager.findByName(username);
+        if (systemUser != null) {
+            String permissions = usermanager.findUserPermissions(systemUser.getUsername());
+            boolean notLocked = false;
+            if (StringUtils.equals(SystemUser.STATUS_VALID, systemUser.getStatus())) {
+                notLocked = true;
+            }
+            FebsAuthUser authUser = new FebsAuthUser(systemUser.getUsername(), systemUser.getPassword(),
+                    true, true, true, notLocked,
+                    AuthorityUtils.commaSeparatedStringToAuthorityList(permissions));
+            BeanUtils.copyProperties(systemUser, authUser);
+            return authUser;
+        } else {
+            throw new UsernameNotFoundException("");
+        }
     }
 }
