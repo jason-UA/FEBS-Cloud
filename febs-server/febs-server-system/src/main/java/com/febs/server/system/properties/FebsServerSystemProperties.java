f18f69175e607d6c8219e5101e1233ad3192268e
jason <Jason.Wang2@harman.com>
2020-05-21 17:14:45 +0800

    swagger auth

---
+++ febs-server/febs-server-system/src/main/java/com/febs/server/system/properties/FebsServerSystemProperties.java
@@ -0,0 +1,17 @@
+package com.febs.server.system.properties;
+
+import lombok.Data;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@SpringBootConfiguration
+@PropertySource(value = {"classpath:febs-server-system.properties"})
+@ConfigurationProperties(prefix = "febs.server.system")
+public class FebsServerSystemProperties {
+
+    private String anonUrl;
+
+    private FebsSwaggerProperties swagger = new FebsSwaggerProperties();
+}
