8bdbd1cfca47b5d5c9225963508f5d10e039dc06
jason <Jason.Wang2@harman.com>
2020-05-22 16:17:22 +0800

    zipkin

--- febs-gateway/src/main/java/com/febs/gateway/filter/FebsGatewayRequestFilter.java
+++ febs-gateway/src/main/java/com/febs/gateway/filter/FebsGatewayRequestFilter.java
@@ -2,19 +2,34 @@ package com.febs.gateway.filter;
 
 
 import com.febs.common.entity.FebsConstant;
+import com.febs.common.entity.FebsResponse;
+import com.febs.common.utils.FebsUtil;
+import com.febs.gateway.properties.FebsGatewayProperties;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.Base64Utils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 @Component
 public class FebsGatewayRequestFilter extends ZuulFilter {
+
+    @Autowired
+    private FebsGatewayProperties properties;
+
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
 
     @Override
     public String filterType() {
@@ -41,6 +56,35 @@ public class FebsGatewayRequestFilter extends ZuulFilter {
         String uri = request.getRequestURI();
 
         log.info("请求URI：{}，HTTP Method：{}，请求IP：{}，ServerId：{}", uri, method, host, serviceId);
+
+        // 禁止外部访问资源实现
+        boolean shouldForward = true;
+        String forbidRequestUri = properties.getForbidRequestUri();
+        String[] forbidRequestUris = StringUtils.splitByWholeSeparatorPreserveAllTokens(forbidRequestUri, ",");
+        if (forbidRequestUris != null && ArrayUtils.isNotEmpty(forbidRequestUris)) {
+            for (String u : forbidRequestUris) {
+                if (pathMatcher.match(u, uri)) {
+                    shouldForward = false;
+                    break;
+                }
+            }
+        }
+        if (!shouldForward) {
+            HttpServletResponse response = context.getResponse();
+            FebsResponse febsResponse = new FebsResponse().message("该URI不允许外部访问");
+            try {
+
+                FebsUtil.makeResponse(
+                        response, MediaType.APPLICATION_JSON_UTF8_VALUE,
+                        HttpServletResponse.SC_FORBIDDEN, febsResponse
+                );
+                context.setSendZuulResponse(false);
+                context.setResponse(response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
         byte[] token = Base64Utils.encode(FebsConstant.ZUUL_TOKEN_VALUE.getBytes());
         context.addZuulRequestHeader(FebsConstant.ZUUL_TOKEN_HEADER, new String(token));
