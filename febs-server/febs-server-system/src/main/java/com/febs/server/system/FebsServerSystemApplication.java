1c3170ea9f753bd404268f54cbf4e86979fd91cc
Jason Wang <Jason.Wang2@harman.com>
2020-05-26 21:31:35 +0800

    nacos

--- febs-server/febs-server-system/src/main/java/com/febs/server/system/FebsServerSystemApplication.java
+++ febs-server/febs-server-system/src/main/java/com/febs/server/system/FebsServerSystemApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @FebsCloudApplication
--- febs-server/febs-server-test/src/main/java/com/febs/server/test/FebsServerTestApplication.java
+++ febs-server/febs-server-test/src/main/java/com/febs/server/test/FebsServerTestApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @FebsCloudApplication
