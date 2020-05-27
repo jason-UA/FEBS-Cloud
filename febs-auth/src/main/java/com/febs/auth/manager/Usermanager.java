82803195f890dc469c30434045798cd2671d9a46
jason <Jason.Wang2@harman.com>
2020-05-27 15:19:29 +0800

    nacos

--- febs-auth/src/main/java/com/febs/auth/manager/Usermanager.java
+++ febs-auth/src/main/java/com/febs/auth/manager/Usermanager.java
@@ -4,12 +4,9 @@ import com.febs.auth.mapper.MenuMapper;
 import com.febs.auth.mapper.UserMapper;
 import com.febs.common.entity.system.Menu;
 import com.febs.common.entity.system.SystemUser;
-import com.netflix.discovery.converters.Auto;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
